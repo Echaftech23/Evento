409ca99b40676ab46254537946231eb3d3b81165
Echaftech23 <rachidchaf2001@gmail.com>
2024-03-04 22:45:46 +0100

    added media library.

--- app/Models/Establishment.php
+++ app/Models/Establishment.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Establishment extends Model
+class Establishment extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'name',
