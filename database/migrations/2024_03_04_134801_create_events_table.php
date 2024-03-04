e12f949a8ea3915d5018ada32c82f6678000577d
Echaftech23 <rachidchaf2001@gmail.com>
2024-03-04 17:54:05 +0100

    added migrations.

---
+++ database/migrations/2024_03_04_134801_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('address');
+            $table->date('registerEndDate');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->integer('capacity');
+            $table->double('price');
+            $table->enum('isAuto', ['0', '1'])->default('0');
+            $table->enum('status', ['0', '1', '2'])->default('0');
+            $table->timestamps();
+            $table->foreignId('city_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('organizer_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
